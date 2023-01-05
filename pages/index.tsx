e2cdbcacdd6fc9051f30eb7cc26739a6b7cd8f56
Ashish jaswal <jaswalashish131@gmail.com>
2023-01-05 15:33:46 +0530

    Component loading added

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,18 @@ import {BsFillMoonStarsFill} from 'react-icons/bs';
 import {AiFillTwitterCircle,AiFillLinkedin,AiFillYoutube} from 'react-icons/ai'
 import Image from "next/image";
 import gshai from '../components/Ginny.png';
-import { useState } from "react";
+import { Suspense, useState } from "react";
+
+import dynamic from "next/dynamic";
+
+const PythonCom = dynamic(()=>import("../components/PythonDev"),
+{loading:()=><p>Loading......</p>})
+
+
 export default function Home() {
   const[darkMode,setDarkMode] = useState(false);
   return (
-    <>
+    <> 
     <div className={darkMode? "dark":""}  >
       
     <Head >
@@ -55,20 +62,9 @@ export default function Home() {
        <Image  src={gshai} alt='Ginny Standing Front'  />
      </div>
       </section>
-      <section>
-        <div className="p-8">
-      <h3 className="text-2xl "><span className="border-4 border-transparent border-b-teal-500
-">Python</span>Dev🐍</h3>
-      <p className="text-md py-2 leading-8 text-gray-600">I am a Python developer and I am currently learning Python. I have been working with <span className="text-teal-500">Python</span> for a while now, but I am always looking to improve my <span className="text-teal-500">skills</span> and stay up-to-date with the latest developments in the language.</p>
-      <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">I am from Dharamshala, Himachal Pradesh and I have a great passion for artificial intelligence. I have always been fascinated by the potential of AI to transform the world and improve people's lives, and I am excited to learn more about this rapidly-evolving field.  </p>
-        </div>
-        
-      </section>
-      <section className="p-8 ">
-        <h3 className="text-2xl"><span className= "border-4 border-transparent border-b-teal-500">My</span> Belief</h3>
-        <p className="text-md py-2 leading-8 text-gray-600">As someone who is deeply interested in the potential of AI to transform the world and improve people's lives, I am constantly seeking out new ways to learn about this rapidly-evolving field. Whether it's through online courses, attending conferences, or simply staying current with the latest research and developments, I am committed to continually expanding my knowledge and expertise in AI and its applications</p>
-        <p className="py-2 text-md leading-8 dark:text-gray-400">I believe that AI has the power to revolutionize industries, solve complex problems, and make a positive impact on society, and I am excited to be part of a community of like-minded individuals who share this passion."</p>
-      </section>
+
+        <PythonCom/>
+
     </main>
     </div>
    
